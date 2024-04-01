23c18ee2e1144d5971f583d1a02bf515d4fe561b
Devetik <sebastien@cuttat.dev>
2024-04-02 00:19:32 +0200

    Version simple optimal

--- Assets/Scripts/MoveToGoalAgent.cs
+++ Assets/Scripts/MoveToGoalAgent.cs
@@ -8,16 +8,20 @@ using Unity.MLAgents.Sensors;
 public class MoveToGoalAgent : Agent
 {
     [SerializeField] private Transform targetTransform;
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material losMaterial;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
 
     public override void OnEpisodeBegin()
     {
-        transform.position = Vector3.zero;
+        transform.localPosition = new Vector3(Random.Range(-8f, +1f), 0, Random.Range(+6f, -5f));
+        targetTransform.localPosition = new Vector3(Random.Range(-8f, +1f), 0, Random.Range(+6f, -5f));
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(transform.position);
-        sensor.AddObservation(targetTransform.position);
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
@@ -28,7 +32,7 @@ public class MoveToGoalAgent : Agent
         float moveZ = actions.ContinuousActions[1];
 
         float moveSpeed = 1f;
-        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -40,15 +44,18 @@ public class MoveToGoalAgent : Agent
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("TRIGGER !!!!");
         if (other.TryGetComponent<Goal>(out Goal goal))
         {
+            Debug.Log("GOOD +1");
             SetReward(+1f);
+            floorMeshRenderer.material = winMaterial;
             EndEpisode();
         }
         if (other.TryGetComponent<Wall>(out Wall wall))
         {
+            Debug.Log("BAD  -1");
             SetReward(-1f);
+            floorMeshRenderer.material = losMaterial;
             EndEpisode();
         }
     }
